07d978580c75c24814cb16c131d0bb37c6a1d6f0
ShubhamYashTechnologies <newgitemail2@gmail.com>
2023-10-10 11:15:57 +0530

    add path for assumptions and dependancy

--- weekly_status/project_plan/urls.py
+++ weekly_status/project_plan/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('risk/', views.RiskView.as_view()),
     path('issue/', views.IssueView.as_view()),
+    path('assumptions/', views.AssumptionsView.as_view()),
+    path('dependancy/', views.DependencyView.as_view()),
 ]
