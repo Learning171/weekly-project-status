caa2c5263f317e6f3111103fb1500f19fbe27481
ShubhamYashTechnologies <newgitemail2@gmail.com>
2023-10-10 10:38:59 +0530

    worked on assumptions and dependancy module

--- weekly_status/project_plan/views.py
+++ weekly_status/project_plan/views.py
@@ -71,3 +71,64 @@ class IssueView(APIView):
         issue = Issue.objects.get(issue_id=request.data["issue_id"])
         issue.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+class AssumptionsView(APIView):
+    def get(self, request):
+        result = Assumptions.objects.all()
+        assumptionsserializer = AssumptionsSerializer(result, many=True)
+        return Response(assumptionsserializer.data, status=status.HTTP_200_OK)
+    
+
+    def post(self, request):
+        data = request.data
+        assumptionsserializer = AssumptionsSerializer(data=data)
+        if assumptionsserializer.is_valid():
+            assumptionsserializer.save()
+            return Response(assumptionsserializer.data, status=status.HTTP_201_CREATED)
+        return Response(assumptionsserializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, format=None):
+        assumptions = Assumptions.objects.get(assumptions_id=request.data["assumptions_id"])
+        serializer = AssumptionsSerializer(assumptions, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, format=None):
+        assumptions = Assumptions.objects.get(assumptions_id=request.data["assumptions_id"])
+        assumptions.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class DependencyView(APIView):
+    def get(self, request):
+        result = Dependency.objects.all()
+        dependencyserializer = DependencySerializer(result, many=True)
+        return Response(dependencyserializer.data, status=status.HTTP_200_OK)
+    
+
+    def post(self, request):
+        data = request.data
+        dependencyserializer = DependencySerializer(data=data)
+        if dependencyserializer.is_valid():
+            dependencyserializer.save()
+            return Response(dependencyserializer.data, status=status.HTTP_201_CREATED)
+        return Response(dependencyserializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, format=None):
+        dependancy = Dependency.objects.get(dependency_id=request.data["dependency_id"])
+        serializer = DependencySerializer(dependancy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, format=None):
+        dependancy = Dependency.objects.get(dependency_id=request.data["dependency_id"])
+        dependancy.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
