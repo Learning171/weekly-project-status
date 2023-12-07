eb49b9f915b321f215b97532512bbc11d3aa34b3
DarshanS001 <97165858+DarshanS001@users.noreply.github.com>
2023-12-07 12:51:10 +0530

    updated reset password link in account/serializers.py

--- weekly_status/account/serializers.py
+++ weekly_status/account/serializers.py
@@ -79,7 +79,7 @@ class SendPasswordResetEmailSerializer(serializers.Serializer):
             user = User.objects.get(user_email=email)
             uid = urlsafe_base64_encode(force_bytes(user.id))
             token = PasswordResetTokenGenerator().make_token(user)
-            link = "http://localhost:3000/api/user/reset/" + uid + "/" + token
+            link = "http://localhost:3000/resetpassword/" + uid + "/" + token
             # Send Email
             body = "Click following link to reset your password " + link
             data = {
